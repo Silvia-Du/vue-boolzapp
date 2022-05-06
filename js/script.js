3e210cb5ff0bfb1707bd31a7cb803f1d09b45dc2
Silvia-Du <silviadu@hotmail.it>
2022-05-06 13:13:01 +0200

    Assegnato active contact al clic del singolo contatto

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@
 Milestone 1
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+Milestone 5 - opzionale
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
+
 */
 
 const app = new Vue({
@@ -174,15 +186,20 @@ const app = new Vue({
         }
     ],
 
+    userProfile: {
+
+        name:'Silvia Dudi',
+        avatar: '_io',
+    },
 
     mexReceived: 'received',
     mexSent: 'sent',
+    activeContact: 1,
   },
 
   methods:{
 
       getIndex(contact){
-        console.log('----');
         const {name, avatar, visible, messages} = contact;
         return contact.messages.length -1;
       }
